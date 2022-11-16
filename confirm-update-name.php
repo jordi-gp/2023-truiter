2e2c339dfc47a2717ba6a991fb4a65d17e4949cd
Jordi García Pons <jordigar00@gmail.com>
2022-11-16 21:59:23 +0100

    Actualitzar el nom en Twitter

---
+++ confirm-update-name.php
@@ -0,0 +1,39 @@
+<?php
+    if($_SERVER["REQUEST_METHOD"] === "POST") {
+        session_start();
+
+        //Connexió a la base de dades
+        require_once 'dbConnection.php';
+
+        $userInf = $_SESSION["user"];
+        $errors = [];
+        $new_name = "";
+
+        if(!empty($_POST["new_name"])) {
+            if(strlen($_POST["new_name"]) > 100) {
+                $errors[] = "El nom no pot contindre més de 100 caracters";
+            } else {
+                $new_name = filter_var($_POST["new_name"], FILTER_SANITIZE_SPECIAL_CHARS);
+            }
+        } else {
+            $errors[] = "El nom no pot estar en blanc!";
+        }
+
+        if(empty($errors)) {
+            $stmt = $pdo->prepare("UPDATE user SET name=:new_name WHERE id=:user_id");
+            $stmt->bindValue('new_name', $new_name);
+            $stmt->bindValue('user_id', $userInf["id"]);
+            $stmt->execute();
+            $_SESSION["user"]["name"] = $new_name;
+            unset($_SESSION["errors"]);
+            header("Location: index.php");
+            exit();
+        } else {
+            $_SESSION["errors"] = $errors;
+            header("Location: edit-name.php");
+            exit();
+        }
+    } else {
+        header('Location: index.php');
+        exit();
+    }
--- edit-name.php
+++ edit-name.php
@@ -1,1 +1,19 @@
 <?php
+    session_start();
+
+    if(isset($_SESSION["logged"])) {
+        //Connexió a la bbdd
+        require_once 'dbConnection.php';
+
+        //Informació de l'usuari
+        $userInfo = $_SESSION["user"];
+
+        if(isset($_SESSION["errors"])) {
+            $errors = $_SESSION["errors"];
+        }
+
+        require 'views/edit-name.view.php';
+    } else {
+        header("Location: index.php");
+        exit();
+    }
