8d05786b33f84ac8ffc713e25de613ad2d04d5cf
Jordi García Pons <jordigar00@gmail.com>
2022-11-18 10:51:10 +0100

    Retoc d'estils

--- views/index.view.php
+++ views/index.view.php
@@ -28,22 +28,50 @@
         <div class="position-fixed col-2 d-flex flex-column justify-content-between h-75">
             <?php require "partials/sidebar.php" ?>
         </div>
+        <!--Buscador de tweets-->
         <div class="offset-2 col-6 border-start border-end border-1 p-4">
             <h1>Welcome to Truiter</h1>
             <p><?=$numOfUsers?> users, <?=$numOfTuits?> tuits</p>
-            <!--Buscador de Tweets-->
-            <div class="mt-2 mb-2">
-                <h2>Buscador de Tweets</h2>
-                <form class="d-flex" method="post" action="find-tweet.php">
-                    <input class="form-control me-2" name="tuit_search" type="search" placeholder="Buscar Tweet..." aria-label="Search">
-                    <button class="btn btn-outline-primary" type="submit">Buscar</button>
-                </form>
-                <?php if(!empty($search_errors)): ?>
-                    <?php foreach($search_errors as $search_err): ?>
-                        <p class="error mt-2"><?=$search_err?></p>
-                    <?php endforeach; ?>
-                <?php endif; ?>
+            <!--Usuaris-->
+            <div class="accordion accordion-flush" id="accordionFlushExample">
+                <div class="accordion-item">
+                    <h2 class="accordion-header" id="flush-headingTwo">
+                        <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseTwo" aria-expanded="false" aria-controls="flush-collapseTwo">
+                            Usuaris
+                        </button>
+                    </h2>
+                    <div id="flush-collapseTwo" class="accordion-collapse collapse" aria-labelledby="flush-headingTwo" data-bs-parent="#accordionFlushExample">
+                        <?php foreach ($users as $user) : ?>
+                            <div class="accordion-body">
+                                <?= $user["name"]; ?> (@<?= $user["username"]; ?>) - Creation date: <?= $user["created_at"]; ?>
+                            </div>
+                        <?php endforeach; ?>
+                    </div>
+                </div>
             </div>
+            <!--Buscador de Tweets-->
+            <div class="accordion accordion-flush" id="accordionFlushExample">
+                <div class="accordion-item">
+                    <h2 class="accordion-header" id="flush-headingOne">
+                        <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseOne" aria-expanded="false" aria-controls="flush-collapseOne">
+                            Buscador de Tweets
+                        </button>
+                    </h2>
+                    <div id="flush-collapseOne" class="accordion-collapse collapse" aria-labelledby="flush-headingOne" data-bs-parent="#accordionFlushExample">
+                        <div class="accordion-body">
+                            <form class="d-flex" method="post" action="find-tweet.php">
+                                <input class="form-control me-2" name="tuit_search" type="search" placeholder="Buscar Tweet..." aria-label="Search">
+                                <button class="btn btn-outline-primary" type="submit">Buscar</button>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <?php if(!empty($search_errors)): ?>
+                <?php foreach($search_errors as $search_err): ?>
+                    <p class="error mt-2"><?=$search_err?></p>
+                <?php endforeach; ?>
+            <?php endif; ?>
             <!--Missatge de benvinguda-->
             <?php if(!empty($info)): ?>
                 <p>Benvingut @<?= $info["username"]?></p>
@@ -55,47 +83,20 @@
                     <?php unset($_SESSION["message"]);?>
                 <?php endif; ?>
             </div>
-            <!--Usuaris-->
-            <div class="accordion accordion-flush" id="accordionFlushExample">
-                <div class="accordion-item">
-                    <h2 class="accordion-header" id="flush-headingOne">
-                        <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseOne" aria-expanded="false" aria-controls="flush-collapseOne">
-                            Usuaris
-                        </button>
-                    </h2>
-                    <div id="flush-collapseOne" class="accordion-collapse collapse" aria-labelledby="flush-headingOne" data-bs-parent="#accordionFlushExample">
-                        <?php foreach ($users as $user) : ?>
-                        <div class="accordion-body">
-                            <?= $user["name"]; ?> (@<?= $user["username"]; ?>) - Creation date: <?= $user["created_at"]; ?>
-                        </div>
-                        <?php endforeach; ?>
-                    </div>
-                </div>
-            </div>
             <!--Tuits-->
-            <div class="accordion accordion-flush" id="accordionFlushExample">
-                <div class="accordion-item">
-                    <h2 class="accordion-header" id="flush-headingTwo">
-                        <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseTwo" aria-expanded="false" aria-controls="flush-collapseTwo">
-                            Tweets
-                        </button>
-                    </h2>
-                    <div id="flush-collapseTwo" class="accordion-collapse collapse" aria-labelledby="flush-headingTwo" data-bs-parent="#accordionFlushExample">
-                        <?php foreach ($tweets as $tweet) : ?>
-                            <div class="accordion-body">
-                                <p><?= $tweet["name"]; ?> (@<?= $tweet["username"]; ?>) - Creation
-                                    date: <?= $tweet["created_at"]; ?></p>
-                                <blockquote><?=$tweet["text"];?></blockquote>
-                                <p>Like counter: <?= $tweet["like_count"] ?></p>
-                                <?php if ($tweet["url"] != null) : ?>
-                                    <img id="imatge" src="<?=$tweet["url"]?>" alt="<?=$tweet["alt_text"]?>" />
-                                <?php endif ;?>
-                                <hr/>
-                            </div>
-                        <?php endforeach; ?>
-                    </div>
+            <h2>Tweets</h2>
+            <?php foreach ($tweets as $tweet) : ?>
+                <div class="accordion-body">
+                    <p><?= $tweet["name"]; ?> (@<?= $tweet["username"]; ?>) - Creation
+                        date: <?= $tweet["created_at"]; ?></p>
+                    <blockquote><?=$tweet["text"];?></blockquote>
+                    <p>Like counter: <?= $tweet["like_count"] ?></p>
+                    <?php if ($tweet["url"] != null) : ?>
+                        <img id="imatge" src="<?=$tweet["url"]?>" alt="<?=$tweet["alt_text"]?>" />
+                    <?php endif ;?>
+                    <hr/>
                 </div>
-            </div>
+            <?php endforeach; ?>
         </div>
         <div class="col-4"></div>
     </div>
